0d6156d11c328ace0734c8289f785016ade4a69b
tatsuta-i <kh_pm_ge@yahoo.co.jp>
2022-11-10 19:48:34 +0900

    fix test.py

--- test.py
+++ test.py
@@ -14,36 +14,41 @@ tmp_file = "" #再生する音のファイルをpianoと番号で指定
 id = Value('i', 1) #自分のid, 接続順により変化 スレッド内でも共通の変数なため、スレッド内の変更が大本のwhile文にも影響する
 key = 0 #他の鍵盤の情報を受け取る
 flag = 0 #レーザーを遮り続けたときに、音を鳴らし続けないように(レーザーを断続的に遮ったときのみ音がなる)
-pool = ThreadPoolExecutor(max_workers=1) #スレッドプールを1にすることで、while文に組み込んでも1つのスレッドまでしか動かない(2にして送信と受信両方のスレッドを動かすかも)
+pool = ThreadPoolExecutor(max_workers=8) #スレッドプールを1にすることで、while文に組み込んでも1つのスレッドまでしか動かない(2にして送信と受信両方のスレッドを動かすかも)
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(pin_in, GPIO.IN)
 
 #場合によっては入力ピンが浮いている状態を回避する必要がある
 #->プルアップ/プルダウン抵抗を指定する必要がある(浮いてるときにON/OFF)
-GPIO.setup(pin_in, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-# GPIO.setup(pin_in, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+#GPIO.setup(pin_in, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(pin_in, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+wav_obj = simpleaudio.WaveObject.from_wave_file("piano/1.wav")
 
 def thread_test(): #スレッドの動作テスト用, 横通信のスレッドに置き換える箇所, スレッドプールの機能により、ここでのtime.sleepの長さでidの振り分け間隔を決めることが可能
-    print(id.value)
-    id.value += 0
-    time.sleep(5)
-
+    #print(id.value)
+    #id.value += 0
+    #time.sleep(5)
+    wav_obj.play()
+    time.sleep(3)
+    
 while True:
 	try:
-		pool.submit(thread_test) #プールにスレッドの関数を渡す
+		#pool.submit(thread_test) #プールにスレッドの関数を渡す
 		tmp_file = piano + str(id.value) + wav #wavファイルをidにて指定
 		#print(tmp_file)
-		if GPIO.input(pin_in) == 1: #レーザーを遮ったとき
-			print("1")
+		if GPIO.input(pin_in) == 0: #レーザーを遮ったとき
+			#print("0")
 			if flag == 0: #前回の判定のときにレーザーが遮られていないとき鳴らす
-				wav_obj = simpleaudio.WaveObject.from_wave_file(tmp_file)
-				wav_obj.play()
-			flag = 0 #これにより、今回鳴らしたことを次回以降で参照可能
+				#wav_obj = simpleaudio.WaveObject.from_wave_file(tmp_file)
+				#wav_obj.play()
+				pool.submit(thread_test)
+			flag = 1 #これにより、今回鳴らしたことを次回以降で参照可能
 		else: #レーザーが照射されているとき
-			print("0")
+			#print("1")
 			flag = 0 #遮ったら音がなる
-		time.sleep(0.39)
+		time.sleep(0.1)
+		print(threading.active_count())
 	except KeyboardInterrupt:
 		GPIO.cleanup()
 		sys.exit()
